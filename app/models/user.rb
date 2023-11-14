2840ef5db7f53a846df1f0fbc44a282150de22ff
Milosz Ganski <valdyrhjord@gmail.com>
2023-11-14 16:06:42 +0100

    Correction of user and profile creation. Problems with missing bio and username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,32 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :profile
+  has_one :profile, inverse_of: :user
   accepts_nested_attributes_for :profile
 
   after_create :create_user_profile
 
   private
+
   def create_user_profile
-    create_profile
+    Rails.logger.debug("=== Create User Profile Params ===")
+
+    profile_params = profile || {}
+
+    existing_profile = profile
+
+    if existing_profile
+      existing_profile.update(username: profile_params[:username], bio: profile_params[:bio])
+      Rails.logger.debug("=== Profile successfully updated ===")
+    else
+      new_profile = build_profile(username: profile_params[:username], bio: profile_params[:bio])
+
+      if new_profile.save
+        Rails.logger.debug("=== Profile successfully saved ===")
+      else
+        Rails.logger.error("=== Error saving profile ===")
+        Rails.logger.error(new_profile.errors.full_messages.inspect)
+      end
+    end
   end
 end
